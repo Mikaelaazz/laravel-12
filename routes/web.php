15bc9f44298a89b6d5a8109a3fa2ff9f93587bcd
Mikaela <johaneswijaya341@gmail.com>
2025-04-14 23:42:50 +0700

    adjust routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StarboyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/starboys', [StarboyController::class, 'index'])->name('starboys.index');
+Route::get('/starboys/create', [StarboyController::class, 'create'])->name('starboys.create');
+Route::post('/starboys', [StarboyController::class, 'store'])->name('starboys.store');
+Route::get('/starboys/{starboy}', [StarboyController::class, 'show'])->name('starboys.show');
+Route::get('/starboys/{starboy}/edit', [StarboyController::class, 'edit'])->name('starboys.edit');
+Route::put('/starboys/{starboy}', [StarboyController::class, 'update'])->name('starboys.update');
+Route::delete('/starboys/{starboy}', [StarboyController::class, 'destroy'])->name('starboys.destroy');
 
